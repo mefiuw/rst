adbe34c38f01984d512faa2df938e080524d1589
mefiuw <wytrwalmateusz@gmail.com>
2021-10-27 11:03:47 +0200

    juz

--- main.js
+++ main.js
@@ -11,50 +11,42 @@ const items = [
 const itemsMeat = items.map((item) => {
     return item.count
 })
-console.log(itemsMeat)
-let item = items[Math.floor(Math.random() * items.length)];
-console.log(item)
-
-
-
-
-let number = 0;
+let i = 1;
+let number = 1;
 let restauracja = document.querySelector('#root')
 let btn = document.querySelector('#addTable')
 
-
 let addTable = () => {
-
-    let div = document.createElement("div");
-    restauracja.appendChild(div)
-
-    let div1 = document.createElement("button")
-    div1.innerHTML = "Hide";
-    div.appendChild(div1)
-
-    let div2 = document.createElement("button")
-    div.appendChild(div2)
-    div2.innerHTML = "Show ";
-    let tekst = document.createElement("span")
-    div.appendChild(tekst)
-    tekst.innerHTML = ""
-
-    let showMenu = () => {
-        if (number == 0) {
-            let item = items[Math.floor(Math.random() * items.length)];
-            tekst.innerHTML = item.count
-        }
-        number++;
-
-    }
-    let hideMenu = () => {
+    if (i <= 4) {
+        let div = document.createElement("div");
+        restauracja.appendChild(div)
+        let div1 = document.createElement("button")
+        div1.innerHTML = "Hide";
+        div.appendChild(div1)
+        let div2 = document.createElement("button")
+        div.appendChild(div2)
+        div2.innerHTML = "Show ";
+        let tekst = document.createElement("span")
+        div.appendChild(tekst)
         tekst.innerHTML = ""
-        number = 0;
+        let showMenu = () => {
+            number == 1;
+            if (number <= 1) {
+                let item = items[Math.floor(Math.random() * items.length)];
+                tekst.innerHTML = item.count
 
+            }
+
+        }
+        let hideMenu = () => {
+            tekst.innerHTML = ""
+            number = 0;
+
+        }
+
+        i++;
+        div1.addEventListener("click", hideMenu);
+        div2.addEventListener("click", showMenu);
     }
-    div1.addEventListener("click", hideMenu);
-    div2.addEventListener("click", showMenu);
 }
-
 btn.addEventListener("click", addTable);
-
