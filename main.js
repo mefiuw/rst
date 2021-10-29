8cb5bdd11ebfa930e4ea7ba3b4e6ff69f75866d8
mefiuw <wytrwalmateusz@gmail.com>
2021-10-29 13:12:42 +0200

    nowy

--- main.js
+++ main.js
@@ -1,52 +1,97 @@
-
-const items = [
-    { result: 'tableA', count: 'obiadA' },
-    { result: 'tableB', count: 'obiadB' },
-    { result: 'tableC', count: 'obiadC' },
-    { result: 'tableD', count: 'obiadD' },
-    { result: 'tableE', count: 'obiadE' },
-    { result: 'tableF', count: 'obiadF' }
-
-]
-const itemsMeat = items.map((item) => {
-    return item.count
-})
+let items = {
+    A: { order: ["sałatka A", "Zupa A", "Deser A"] },
+    B: { order: ["sałatka B", "Zupa B", "Deser B"] },
+    C: { order: ["sałatka C", "Zupa C", "Deser C"] },
+    D: { order: ["sałatka D", "Zupa D", "Deser D"] },
+}
+let a = [1, 2, 3, 4];
 let i = 1;
 let number = 1;
+let number1 = 1;
 let restauracja = document.querySelector('#root')
 let btn = document.querySelector('#addTable')
 
 let addTable = () => {
+
     if (i <= 4) {
-        let div = document.createElement("div");
+        let div = document.createElement("div")
+        div.setAttribute("id", i);
         restauracja.appendChild(div)
         let div1 = document.createElement("button")
+        div1.setAttribute("id", i);
         div1.innerHTML = "Hide";
         div.appendChild(div1)
         let div2 = document.createElement("button")
+        div2.setAttribute("id", i);
         div.appendChild(div2)
         div2.innerHTML = "Show ";
         let tekst = document.createElement("span")
         div.appendChild(tekst)
+        tekst.setAttribute("id", i);
         tekst.innerHTML = ""
+        let close = document.createElement("button")
+        div.appendChild(close)
+        close.setAttribute("id", i);
+        close.innerHTML = "Close";
+        console.log(i)
+
+
         let showMenu = () => {
-            number == 1;
-            if (number <= 1) {
-                let item = items[Math.floor(Math.random() * items.length)];
-                tekst.innerHTML = item.count
-                number++;
+
+            if (tekst.id == "1") {
+
+                tekst.innerHTML = items.A.order;
+            }
+            if (tekst.id == "2") {
+
+                tekst.innerHTML = items.B.order;
+            } if (tekst.id == "3") {
+
+                tekst.innerHTML = items.C.order;
+            } if (tekst.id == "4") {
+
+                tekst.innerHTML = items.D.order;
+            }
+        }
+        let closeDiv = () => {
+
+            if (close.id == 1) {
+                div.remove();
+
+            }
+            if (close.id == 2) {
+                div.remove();
+
+            }
+            if (close.id == 3) {
+                div.remove();
+
+            }
+            if (close.id == 4) {
+                div.remove();
+
+            }
+            i--;
+        }
+        close.addEventListener('click', closeDiv);
+
+        let hideMenu = () => {
+            if (number1 == 1) {
+                tekst.innerHTML = ""
+
             }
 
-        }
-        let hideMenu = () => {
-            tekst.innerHTML = ""
-            number = 0;
 
         }
 
         i++;
         div1.addEventListener("click", hideMenu);
         div2.addEventListener("click", showMenu);
+
     }
 }
+
 btn.addEventListener("click", addTable);
+
+
+
