6047ffc8a16f337dd4b642825ef85b1fe882ca88
mefiuw <wytrwalmateusz@gmail.com>
2021-11-17 09:30:34 +0100

    new

--- main.js
+++ main.js
@@ -4,115 +4,115 @@ let items = {
     C: { order: ["sałatka C", "Zupa C", "Deser C"] },
     D: { order: ["sałatka D", "Zupa D", "Deser D"] },
 }
-let a = 1;
-
-let number = 1;
+const tab = [tableA = 0, tableB = 1, tableC = 2, tableD = 3]
+let index = [0];
 let number1 = 1;
 let restauracja = document.querySelector('#root')
 let btn = document.querySelector('#addTable')
-
-let i = 1;
-
-
-
+let i = 0;
+let target
 
 let addTable = () => {
 
+    if (i >= 4 || tab.length < 4 || index == 5) {
+        btn.style.display = "none";
+        restauracja.style.backgroundColor = "red";
+        alert("nie masz tylu klientów, nie żartuj sobie");
+        target.removeEventListener("click", addTable)
 
-    if (i <= 4) {
-        if (a > 4) {
-            alert('osiągnieto max ilosc stolikow')
-            a = 5;
-            i = 5;
-            return;
-        }
+    }
+
+
+
+
+    if (!tableA) {
         let div = document.createElement("div")
-        div.setAttribute("id", i, a);
-        restauracja.appendChild(div)
+        div.setAttribute("id", index);
+        restauracja.appendChild(div);
         let div1 = document.createElement("button")
-        div1.setAttribute("id", i, a);
+        div1.setAttribute("id", index)
         div1.innerHTML = "Hide";
         div.appendChild(div1)
         let div2 = document.createElement("button")
-        div2.setAttribute("id", i, a);
-        div.appendChild(div2)
+        div2.setAttribute("id", index)
         div2.innerHTML = "Show ";
+        div.appendChild(div2)
         let tekst = document.createElement("span")
-        div.appendChild(tekst)
-        tekst.setAttribute("id", i, a);
+        tekst.setAttribute("id", index)
         tekst.innerHTML = ""
+        div.appendChild(tekst)
         let close = document.createElement("button")
-        div.appendChild(close)
-        close.setAttribute("id", i, a);
+        close.setAttribute("id", index)
         close.innerHTML = "Close";
-        console.log(i, a)
-
-
+        div.appendChild(close)
+        let test = Math.floor(Math.random() * 4)
+        //console.log(test);
         let showMenu = () => {
 
-            if (tekst.id == 1) {
-
+            if (test === 0)// || tekst.id == [tableA])
+            {
                 tekst.innerHTML = items.A.order;
             }
-            if (tekst.id == 2) {
+            if (test === 1) { //|| tekst.id == [tableB]) {
 
                 tekst.innerHTML = items.B.order;
-            } if (tekst.id == 3) {
+            } if (test === 2) {//|| tekst.id == [tableC]) {
 
                 tekst.innerHTML = items.C.order;
-            } if (tekst.id == 4) {
+            } if (test === 3) { //|| tekst.id == [tableD]) {
 
                 tekst.innerHTML = items.D.order;
             }
         }
-        let closeDiv = (e) => {
-
-            if (close.id == 1) {
-                div.remove(1);
-                i = 1;
-
-
-
-            }
-            if (close.id == 2) {
-                div.remove(2);
-                i = 2;
-
-
-            }
-            if (close.id == 3) {
-                div.remove(3);
-                i = 3;
-            }
-            if (close.id == 4) {
-                div.remove(4);
-                i = 4;
-
-            }
-
-            a--;
-        }
-        close.addEventListener('click', closeDiv);
-
+        div2.addEventListener("click", showMenu);
         let hideMenu = () => {
             if (number1 == 1) {
                 tekst.innerHTML = ""
-
             }
 
+        }
+        div1.addEventListener("click", hideMenu);
+        let closeDiv = () => {
+
+
+            if (close.id == tableA) {
+                div.remove(tableA);
+                index--;
+            }
+            else index == tableA;
+            div.remove(tableA);
+            if (close.id == tableB) {
+                div.remove(tableB);
+                index--;
+
+            }
+            else index == tableB;
+            div.remove(tableB);
+            if (close.id == tableC) {
+                div.remove(tableC);
+                index--;
+            }
+            else index == tableC;
+            div.remove(tableC);
+            if (close.id == tableD) {
+                div.remove(tableD);
+                index--;
+            } else index == tableD;
+            div.remove(tableD);
+            btn.style.display = "block";
+            restauracja.style.backgroundColor = "purple";
+
+            i--;
 
         }
-
-        i++;
-        a++;
-        div1.addEventListener("click", hideMenu);
-        div2.addEventListener("click", showMenu);
-
+        close.addEventListener('click', closeDiv);
     }
 
+    //console.log(i, index);
+    i++;
+    index++;
 }
-btn.addEventListener("click", addTable);
-
+btn.addEventListener('click', addTable)
 
 
 
